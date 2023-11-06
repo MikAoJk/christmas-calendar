1b3e8cb211ccc9cd4e3c84e200bace4cd9bde78b
MikAoJk <joakimkartveit@gmail.com>
2023-11-06 07:27:11 +0000

    Deploying to gh-pages from @ MikAoJk/advent-calendar@6ab36e635297220e1f2834f0acf6c9b0b8d01c22 🚀

--- _next/static/lmtQGK7321yNgmsbUBU2c/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/098752cf92351fc3.css","static/chunks/pages/index-1e1269d231068e79.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
